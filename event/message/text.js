5bf624409cceb73618e306bcae0ef1a1a1cc82b2
watanabe_atsuya <watanabe_atuya@plise.co.jp>
2022-08-03 19:17:04 +0900

    event一覧修正

--- event/message/text.js
+++ event/message/text.js
@@ -20,18 +20,18 @@ export const textEvent = async (event, client) => {
   }
   // テキストメッセージの処理をする関数
   let contextData;
-  let memoData;
   let memberData;
+  let eventMemoData
   try {
     contextData = contextDB.getData(`/${userId}/context`);
   } catch (_) {
     contextData = undefined;
   }
-  // try {
-  //   memoData = eventDB.getData(`/${userId}/event/`);
-  // } catch (_) {
-  //   memoData = undefined;
-  // }
+  try {
+    eventMemoData = eventDB.getData(`/${userId}/event/`);
+  } catch (_) {
+    eventMemoData = undefined;
+  }
   // try {
   //   memoData = messageDB.getData(`/${userId}/memo`);
   // } catch (_) {
